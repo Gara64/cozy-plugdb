51765368a225d9edfcfc0832e8b2940c96d43b63
Gara64 <paul.tranvan@gmail.com>
2015-01-12 23:50:16 +0100

    add the creation of couchdb doc through the datasystem

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 // Generate a new instance of express server.
 var express = require('express')
   , http = require('http')
+  , Client = require('request-json').JsonClient
   , plug = require('./lib/plug.js');
 
 var app = express();
+
+// The data system listens to localhost:9101
+dataSystem = new Client('http://localhost:9101');
+
+// In production we must authentificate the application
+/*if(process.env.NODE_ENV === 'production') {
+  user = process.env.NAME;
+  password = process.env.TOKEN;
+  dataSystem.setBasicAuth(user, password);
+}*/
+
+
+
 
 /* This will allow Cozy to run your app smoothly but
  it won't break other execution environment */
@@ -19,6 +33,25 @@ var server = http.createServer(app).listen(port, host, function() {
 // At the root of your website, we show the index.html page
 app.get('/', function(req, res) {
   res.sendfile('./public/index.html')
-  plug.start();
-
+  //plug.start();
 });
+
+var data = {'test' : 'testy'} ;
+
+// The request must be created first, let's say it is
+var post = function() {
+	dataSystem.post('data/', data, function(err, res, body) {
+	  if(err !== null || (res !== null && res.statusCode != 201)) {
+	    if(res !== null) 
+	    	code = res.statusCode; 
+	    else 
+	    	code = "no status code";
+	    console.log("An error occurred -- [" + code + "] " + err);
+	  }
+	  else {
+	    console.log("created doc : ");
+	    console.log(body);
+	  }
+	});
+}
+

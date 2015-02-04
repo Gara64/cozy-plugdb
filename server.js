89f07cde1acf6778b13e91a0205744abeae4460b
Gara64 <paul.tranvan@gmail.com>
2015-02-04 16:41:03 +0100

    Change port and fix models bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var americano = require('americano');
 
-var port = process.env.PORT || 9120;
-americano.start({name: 'template', port: port}, function(app) {
+var port = process.env.PORT || 9999;
+americano.start({name: 'plug', port: port}, function(app) {
     app.set('views', __dirname + '/client');
     app.engine('.html', require('jade').__express);
 });
--- server/models/requests.js
+++ server/models/requests.js
@@ -1,23 +1,7 @@
-/* See documentation on
- https://github.com/frankrousseau/americano-cozy/#requests */
-
-var americano = require('americano');
+americano = require('americano');
 
 module.exports = {
-  template: {
-    // shortcut for emit doc._id, doc
-    all: americano.defaultRequests.all,
-
-    /* create all the requests you want!
-    This request will gives you the number of documents that share
-    the same date */
-    customRequest: {
-      map: function (doc) {
-        return emit(doc.date, doc);
-      },
-      reduce: function (key, values, rereduce) {
-        return sum(values);
-      }
+    contacts: {
+        all: americano.defaultRequests.all
     }
-  }
 };
